1b1625612fa5fddea5d9c52b5f76c72058e9e4d0
ChiaraMameli <mameli.chiara@libero.it>
2022-07-25 15:13:30 +0200

    refactoring header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BaseHeader />
+    <BaseHeader :header-links= "HeaderLinks"/>
     <BaseMain />
     <BonusSection />
     <BaseFooter />
@@ -20,6 +20,62 @@ export default {
     BaseMain,
     BaseFooter,
     BonusSection
+},
+data(){
+  return {
+      HeaderLinks: [
+          {
+            text: 'Characters',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'Comics',
+            url: '#',
+            current: true,
+          },
+          {
+            text: 'Movies',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'TV',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'Games',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'Collectibles',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'Videos',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'Fans',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'News',
+            url: '#',
+            current: false,
+          },
+          {
+            text: 'Shop',
+            url: '#',
+            current: false,
+          },
+             ]
+  }
 }
 }
 </script>
--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -4,10 +4,9 @@
             <a href="#">
                 <img src="../assets/img/dc-logo.png" alt="dc logo">
             </a>
-
             <div id="nav-links">
                 <ul>
-                    <li v-for="link in NavBar" v-bind:key="link.text" :class="{'active' : link.current}"><a :href="link.url">{{link.text}}</a></li>
+                    <li v-for="link in HeaderLinks" v-bind:key="link.text" :class="{'active' : link.current}"><a :href="link.url">{{link.text}}</a></li>
                 </ul>
             </div>
         </nav>
@@ -17,62 +16,8 @@
 <script>
 export default {
     name: 'BaseHeader',
-    data(){
-    return{
-        NavBar: [
-  {
-    text: 'Characters',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'Comics',
-    url: '#',
-    current: true,
-  },
-  {
-    text: 'Movies',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'TV',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'Games',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'Collectibles',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'Videos',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'Fans',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'News',
-    url: '#',
-    current: false,
-  },
-  {
-    text: 'Shop',
-    url: '#',
-    current: false,
-  },
-]
-
-    }
+    props: {
+      HeaderLinks: HeaderLinks,
     }
 }
 </script>
