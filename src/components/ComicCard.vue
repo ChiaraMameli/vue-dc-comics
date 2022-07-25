cb0f198def1b037126f6362965c9059b53de3411
ChiaraMameli <mameli.chiara@libero.it>
2022-07-25 16:46:25 +0200

    comic card is a component

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div  class="comic-card">
+        <figure>
+            <img :src="comic['thumb']" :alt="comic['series']">
+        </figure>
+        <figcaption>{{comic['series']}}</figcaption>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComicCard',
+    props: {
+        comic: Object,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .comic-card{
+    flex-basis: calc((100% / 6) - 20px);
+    padding-bottom: 50px;
+
+
+    img{
+        height: 200px;
+        width: 200px;
+        object-fit: cover;
+        object-position: 0% 0%;
+    }
+
+    figcaption{
+        text-transform: capitalize;
+    }
+    }
+
+</style>
